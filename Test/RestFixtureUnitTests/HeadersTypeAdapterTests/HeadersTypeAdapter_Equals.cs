dff8d23ba832fa39922a7259d893403f7b1dd7bb
SimonE <SimonE@datacom.co.nz>
2017-09-13 16:51:10 +1200

    Add unit tests for HeaderTypeAdapter class

---
+++ Test/RestFixtureUnitTests/HeadersTypeAdapterTests/HeadersTypeAdapter_Equals.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using restFixture.Net.TypeAdapters;
+using RestClient.Data;
+
+namespace RestFixtureUnitTests.HeadersTypeAdapterTests
+{
+    [TestClass]
+    public class HeadersTypeAdapter_Equals
+    {
+        private HeadersTypeAdapter _adapter = new HeadersTypeAdapter();
+
+        [TestMethod]
+        public void Should_Return_False_For_Null_Equality()
+        {
+            // Act.
+            bool actualResult = _adapter.Equals(null, null);
+
+            // Assert.
+            Assert.IsFalse(actualResult);
+        }
+
+        [TestMethod]
+        public void Should_Return_True_When_Actual_Headers_Identical_To_Expected()
+        {
+            // Arrange.
+            IList<RestData.Header> expectedHeaders = GetHeadersList(3);
+            IList<RestData.Header> actualHeaders = GetHeadersList(3);
+
+            // Act.
+            bool actualResult = _adapter.Equals(expectedHeaders, actualHeaders);
+
+            // Assert.
+            Assert.IsTrue(actualResult);
+        }
+
+        [TestMethod]
+        public void Should_Return_True_When_Actual_Headers_Identical_But_Different_Order()
+        {
+            // Arrange.
+            IList<RestData.Header> expectedHeaders = 
+                GetHeadersList(3).OrderByDescending(h => h.Name).ToList();
+            IList<RestData.Header> actualHeaders = GetHeadersList(3);
+
+            // Act.
+            bool actualResult = _adapter.Equals(expectedHeaders, actualHeaders);
+
+            // Assert.
+            Assert.IsTrue(actualResult);
+        }
+
+        [TestMethod]
+        public void Should_Return_True_When_Actual_Headers_Superset_Of_Expected()
+        {
+            // Arrange.
+            IList<RestData.Header> expectedHeaders = GetHeadersList(3);
+            IList<RestData.Header> actualHeaders = GetHeadersList(5);
+
+            // Act.
+            bool actualResult = _adapter.Equals(expectedHeaders, actualHeaders);
+
+            // Assert.
+            Assert.IsTrue(actualResult);
+        }
+
+        [TestMethod]
+        public void Should_Return_False_When_Actual_Headers_Subset_Of_Expected()
+        {
+            // Arrange.
+            IList<RestData.Header> expectedHeaders = GetHeadersList(3);
+            IList<RestData.Header> actualHeaders = GetHeadersList(2);
+
+            // Act.
+            bool actualResult = _adapter.Equals(expectedHeaders, actualHeaders);
+
+            // Assert.
+            Assert.IsFalse(actualResult);
+        }
+
+        [TestMethod]
+        public void Should_Match_Expected_Header_Containing_Regex()
+        {
+            // Arrange.
+            RestData.Header expectedContentTypeHeaderWithRegex =
+                new RestData.Header("content-type", "application/my-xml;.+");
+            RestData.Header actualContentTypeHeader =
+                new RestData.Header("content-type", "application/my-xml;charset=UTF-8");
+
+            IList<RestData.Header> expectedHeaders =
+                new List<RestData.Header> { expectedContentTypeHeaderWithRegex };
+            IList<RestData.Header> actualHeaders =
+                new List<RestData.Header> { actualContentTypeHeader };
+
+            // Act.
+            bool actualResult = _adapter.Equals(expectedHeaders, actualHeaders);
+
+            // Assert.
+            Assert.IsTrue(actualResult);
+        }
+
+        private IList<RestData.Header> GetHeadersList(int numberOfHeaders)
+        {
+            IList<RestData.Header> headerList = new List<RestData.Header>();
+
+            for (int i = 0; i < numberOfHeaders; i++)
+            {
+                headerList.Add(new RestData.Header("name" + i, "value" + i));
+            }
+
+            return headerList;
+        }
+    }
+}
---
+++ Test/RestFixtureUnitTests/HeadersTypeAdapterTests/HeadersTypeAdapter_Parse.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using restFixture.Net.TypeAdapters;
+using RestClient.Data;
+
+namespace RestFixtureUnitTests.HeadersTypeAdapterTests
+{
+    [TestClass]
+    public class HeadersTypeAdapter_Parse 
+    {
+        private HeadersTypeAdapter _adapter = new HeadersTypeAdapter();
+
+        [TestMethod]
+        public void Should_Parse_Headers_From_Html_String()
+        {
+            // Arrange.
+            IList<RestData.Header> expectedHeaders = new List<RestData.Header>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                expectedHeaders.Add(new RestData.Header("n" + i, "v" + i));
+            }
+
+            string headersHtmlString = " n0 : v0 <br/> n1: v1 <br  /> n2 :v2 ";
+            IList<RestData.Header> parsedHeaders =
+                (IList<RestData.Header>)_adapter.parse(headersHtmlString);
+
+            // Act.
+            bool result = _adapter.Equals(expectedHeaders, parsedHeaders);
+
+            // Assert.
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Should_Parse_Headers_From_Html_String_Containing_Colons()
+        {
+            // Arrange.
+            IList<RestData.Header> expectedHeaders = new List<RestData.Header>();
+            expectedHeaders.Add(new RestData.Header("n", "http://something:port/blah:blah"));
+            expectedHeaders.Add(new RestData.Header("n1", "v1"));
+            string headersHtmlString = " n : http://something:port/blah:blah <br/> n1: v1 ";
+            IList<RestData.Header> parsedHeaders =
+                (IList<RestData.Header>)_adapter.parse(headersHtmlString);
+
+            // Act.
+            bool result = _adapter.Equals(expectedHeaders, parsedHeaders);
+
+            // Assert.
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Should_Parse_Headers_From_Html_String_Containing_QueryString()
+        {
+            // Arrange.
+            IList<RestData.Header> expectedHeaders = new List<RestData.Header>();
+            expectedHeaders.Add(new RestData.Header("n", "http://something:port/blah/1?blah=1&other=2"));
+            expectedHeaders.Add(new RestData.Header("n1", "v1"));
+            string headersHtmlString = " n : http://something:port/blah/1?blah=1&other=2 <br/> n1: v1 ";
+            IList<RestData.Header> parsedHeaders =
+                (IList<RestData.Header>)_adapter.parse(headersHtmlString);
+
+            // Act.
+            bool result = _adapter.Equals(expectedHeaders, parsedHeaders);
+
+            // Assert.
+            Assert.IsTrue(result);
+        }
+    }
+}
