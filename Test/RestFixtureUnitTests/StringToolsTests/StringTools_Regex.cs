5482c2b5d0da4c877f677d69cb2cd6720e28004d
SimonE <SimonE@datacom.co.nz>
2017-09-13 16:44:24 +1200

    Rename StringTools test classes to match class under test

--- Test/RestFixtureUnitTests/StringToolsTests/StringTools_Regex.cs
+++ Test/RestFixtureUnitTests/StringToolsTests/StringTools_Regex.cs
@@ -5,7 +5,7 @@ using restFixture.Net.Tools;
 namespace RestFixtureUnitTests.StringToolsTests
 {
     [TestClass]
-    public class Tools_Regex
+    public class StringTools_Regex
     {
         [TestMethod]
         public void Should_Return_True_When_Regex_Is_Valid_And_Matches_Text()
@@ -39,6 +39,39 @@ namespace RestFixtureUnitTests.StringToolsTests
 
             // Act & Assert.
             StringTools.regex(text, regexPattern);
+        }
+
+        [TestMethod]
+        public void Should_Match_Simple_Url()
+        {
+            // Arrange.
+            string text = "http://domain.com/resource";
+            string regexPattern = "http://domain.com/resource";
+
+            // Act & Assert.
+            Assert.IsTrue(StringTools.regex(text, regexPattern));
+        }
+
+        [TestMethod]
+        public void Should_Match_Url_With_Port()
+        {
+            // Arrange.
+            string text = "http://domain.com:8080/resource";
+            string regexPattern = "http://domain.com:8080/resource";
+
+            // Act & Assert.
+            Assert.IsTrue(StringTools.regex(text, regexPattern));
+        }
+
+        [TestMethod]
+        public void Should_Match_Url_With_QueryString()
+        {
+            // Arrange.
+            string text = "http://domain.com:8080/resource/1?blah=1&other=2";
+            string regexPattern = @"http://domain.com:8080/resource/1\?blah=1&other=2";
+
+            // Act & Assert.
+            Assert.IsTrue(StringTools.regex(text, regexPattern));
         }
     }
 }
